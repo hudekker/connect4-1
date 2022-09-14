b194df259cab8c0eaf60bec403ae51dd9d5fae7c
robert hudek <hudektech@gmail.com>
2022-09-14 20:14:13 +0800

    5

--- script-constants.js
+++ script-constants.js
@@ -1,5 +1,5 @@
-const MSG1 = `Player One's Turn (green).  Click on top row to drop chip`;
-const MSG2 = `Player Two's Turn (red).  Click on top row to drop chip`;
-const MSG3 = `Player One (green) Won!!!  Play Again?`;
-const MSG4 = `Player Two (red) Won!!!  Play Again?`;
+const MSG1 = `Player One's Turn (green)\r\nClick on top row to drop chip`;
+const MSG2 = `Player Two's Turn (red)\r\nClick on top row to drop chip`;
+const MSG3 = `Player One (green) Won!!!\r\nPlay Again?`;
+const MSG4 = `Player Two (red) Won!!!\r\nPlay Again?`;
 const MSG_START_OVER = `Play Again!`;

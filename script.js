1e3389cc1cfcaefd9af366c45806609e5618d296
robert hudek <hudektech@gmail.com>
2022-09-20 19:26:45 +0800

    1

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ circles.forEach((el) => {
       }
 
       boolWin = checkResult(boolGreen, circles, ROWS, COLS);
+
       if (boolWin) {
         msg.classList.add("hidden");
         msg.classList.remove("p2");
@@ -79,65 +80,3 @@ btnStartOver.addEventListener("click", (evt) => {
 
   boolGreen = true;
 });
-// const checkResult = () => {
-//   let playerColor = boolGreen ? "green" : "red";
-//   let arr = new Int8Array(circles.filter((el) => el.classList.contains(playerColor)).map((el) => +el.id.slice(1))).sort();
-
-//   // Check horizontal
-//   for (let i = 0; i < arr.length - 3; i++) {
-//     for (let j = 0; j < ROWS; j++) {
-//       for (let k = 0; k < COLS - 3; k++) {
-//         let bool0 = arr[i] == j * COLS + k;
-//         let bool1 = arr[i + 1] == j * COLS + k + 1;
-//         let bool2 = arr[i + 2] == j * COLS + k + 2;
-//         let bool3 = arr[i + 3] == j * COLS + k + 3;
-//         if (bool0 && bool1 && bool2 && bool3) {
-//           alert("you won!");
-//         }
-//       }
-//     }
-//   }
-
-//   // Check vertical
-//   for (let i = 0; i < arr.length - 3; i++) {
-//     for (let j = 0; j < COLS; j++) {
-//       for (let k = 0; k < ROWS - 3; k++) {
-//         let bool0 = arr[i] == j + k * COLS;
-//         let bool1 = arr[i + 1] == j + (k + 1) * COLS;
-//         let bool2 = arr[i + 2] == j + (k + 2) * COLS;
-//         let bool3 = arr[i + 3] == j + (k + 3) * COLS;
-//         if (bool0 && bool1 && bool2 && bool3) {
-//           alert("you won!");
-//         }
-//       }
-//     }
-//   }
-
-//   // Check upward diagonal
-//   for (let i = 0; i < ROWS; i++) {
-//     for (let j = 0; j < COLS - 3; j++) {
-//       let bool0 = arr.includes(i * COLS + j + 0 * (COLS + 1));
-//       let bool1 = arr.includes(i * COLS + j + 1 * (COLS + 1));
-//       let bool2 = arr.includes(i * COLS + j + 2 * (COLS + 1));
-//       let bool3 = arr.includes(i * COLS + j + 3 * (COLS + 1));
-//       if (bool0 && bool1 && bool2 && bool3) {
-//         alert("you won!");
-//       }
-//     }
-//   }
-
-//   // Check downward diagonal
-//   for (let i = 0; i < ROWS; i++) {
-//     for (let j = COLS - 1; j > COLS - 5; j--) {
-//       let bool0 = arr.includes(i * COLS + j + 0 * (COLS - 1));
-//       let bool1 = arr.includes(i * COLS + j + 1 * (COLS - 1));
-//       let bool2 = arr.includes(i * COLS + j + 2 * (COLS - 1));
-//       let bool3 = arr.includes(i * COLS + j + 3 * (COLS - 1));
-//       if (bool0 && bool1 && bool2 && bool3) {
-//         alert("you won!");
-//       }
-//     }
-//   }
-
-//   console.log(arr);
-// };
